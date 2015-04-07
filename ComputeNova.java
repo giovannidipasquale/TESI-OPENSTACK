1dc185fb5ad69eb606255958b1b48d02cbad90b0
giovannidipasquale <giovanni.dipasquale89@gmail.com>
2015-04-07 13:20:13 +0200

    Create ComputeNova.java

---
+++ ComputeNova.java
@@ -0,0 +1,13 @@
+public abstract class ComputeNova 
+{
+	public ComputeNova() {}
+	
+	//CREA SERVER
+	public void CreazioneServer() {}
+	
+	//CANCELLA TUTTI I SERVER
+	public void CancellaTutti() {}
+	
+	//CANCELLA UN SOLO SERVER
+	public void CancellaUnServer() {}
+}
