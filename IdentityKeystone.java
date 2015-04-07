5c2cae59ef4203edaeba5eaade166e396ddda76f
giovannidipasquale <giovanni.dipasquale89@gmail.com>
2015-04-07 13:29:11 +0200

    Create IdentityKeystone.java

---
+++ IdentityKeystone.java
@@ -0,0 +1,10 @@
+public abstract class IdentityKeystone 
+{
+	public IdentityKeystone() {}
+	
+	//CREAZIONE TENANT, USER E ASSEGNAZIONE RUOLO ALL'UTENTE
+	public void CreazioneUtente() {}
+	
+	//AGGIORNA DATI UTENTE
+	public void AggiornaDatiUtente() {}
+}
