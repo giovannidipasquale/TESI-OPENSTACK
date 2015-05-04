4dbeda08c4aed9e6200027c819f7d5ca95e2fb18
Giovanni Antonio Dipasquale <giovanni.dipasquale89@gmail.com>
2015-05-04 23:21:00 +0200

    Update ComputeNova.java

--- ComputeNova.java
+++ ComputeNova.java
@@ -1,13 +1,24 @@
+import java.io.IOException;
+
 public abstract class ComputeNova 
 {
-	public ComputeNova() {}
-	
+	public ComputeNova() 
+	{
+		
+	}
 	//CREA SERVER
-	public void CreazioneServer() {}
-	
+	public void CreazioneServer() throws NumberFormatException, IOException, InterruptedException 
+	{
+		
+	}
 	//CANCELLA TUTTI I SERVER
-	public void CancellaTutti() {}
-	
+	public void CancellaTutti() throws InterruptedException
+	{
+
+	}
 	//CANCELLA UN SOLO SERVER
-	public void CancellaUnServer() {}
+	public void CancellaUnServer() throws NumberFormatException, IOException, InterruptedException
+	{
+
+	}
 }
