e0e63dc03744ad02fa9ba1ba4c5e401d69185830
Giovanni Antonio Dipasquale <giovanni.dipasquale89@gmail.com>
2015-05-04 23:30:13 +0200

    Update GestioneFile.java

--- GestioneFile.java
+++ GestioneFile.java
@@ -8,19 +8,17 @@ import org.openstack4j.model.common.DLPayload;
 import org.openstack4j.model.common.Payloads;
 import org.openstack4j.model.storage.object.SwiftContainer;
 import org.openstack4j.model.storage.object.options.CreateUpdateContainerOptions;
+import org.openstack4j.model.storage.object.options.ObjectLocation;
 import org.openstack4j.model.storage.object.options.ObjectPutOptions;
 
 public class GestioneFile 
 {	
-	public GestioneFile() {}
+	public GestioneFile() 
+	{
+		
+	}
 	
-	//NON FUNZIONA
-	//IL PROBLEMA E' CAPIRE COME COSTRUIRE MEGLIO IL LINK DELLA RICHIESTA GET:
-	//DI DEFAULT FA' SEMPRE HTTP://INDIRIZZO IP:8080/. NOI DOVREMMO FARLO DIVENTARE COME SEGUE:
-	//HTTP://INDIRIZZO IP:8080/v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/
-	//IN QUESTO MODO DOVREBBE DARCI IL LISTING DI TUTTI I CONTAINER 
-	//AUTH Giovanni: AUTH_34cbe4ae849b4ed2b47c40d391d69039
-	//AUTH Giuseppe: AUTH_0613b45860a545608c638f40b6bf7938
+	//VEDERE MEGLIO IL FUNZIONAMENTO
 	//LISTING DEI CONTAINERS
 	public void ListaContainer(OSClient os)
 	{
@@ -31,55 +29,55 @@ public class GestioneFile
 	}
 	
 	//CREAZIONE DI UN CONTAINER
-	public void CreaContainer(OSClient os, String nomeContainer)
+	public void CreaContainer(OSClient os)
 	{
-		ConfigurationProperties props = new ConfigurationProperties();
-		props.getProperty("swiftAccount");
-		os.objectStorage().containers().create("v1/" + props.getProperty("swiftAccount") + "/" + nomeContainer,
+		os.objectStorage().containers().create("v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/Pippo",
 													CreateUpdateContainerOptions.create()
 													.accessAnybodyRead());
-		System.out.println("Container Creato");
     }
 	
 	//CANCELLAZIONE DI UN CONTAINER
-	public void CancellaContainer(OSClient os, String nomeContainer)
+	public void CancellaContainer(OSClient os)
 	{
-		ConfigurationProperties props = new ConfigurationProperties();
-		props.getProperty("swiftAccount");
-		String nomecontainer = "v1/" + props.getProperty("swiftAccount") + "/" + nomeContainer;
+		String nomecontainer = "v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/nomecontainer";
 		os.objectStorage().containers().delete(nomecontainer);
 		System.out.println("Cancellazione avvenuta");
     }
 	
 	//CREAZIONE DELL'OGGETTO
-	public void CreaOggetto(OSClient os, String container, String objectName, String localPath)
+	public void CreaOggetto(OSClient os)
 	{
-		File f = new File(localPath);
-		ConfigurationProperties props = new ConfigurationProperties();
-		props.getProperty("swiftAccount");
-		String etag = os.objectStorage().objects().put(
-            "v1/" + props.getProperty("swiftAccount") + "/" + container, objectName, 
+		String path = "/home/giovannidipasquale/git/Autenticazione Openstack/CiaoMondo.java";
+		File f = new File(path);
+		
+		String etag = os.objectStorage().objects().put("v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/cnt569", "ProvaPUT", 
                 Payloads.create(f), 
                 ObjectPutOptions.create());
-		System.out.println("Oggetto creato con etichetta = " + etag);
+		
+		System.out.println(etag);
 	}
 	
 	//CANCELLAZIONE DELL'OGGETTO
-	public void CancellaOggetto(OSClient os, String container, String objectName)
+	public void CancellaOggetto(OSClient os)
 	{
-		ConfigurationProperties props = new ConfigurationProperties();
-		props.getProperty("swiftAccount");
-		System.out.println(os.objectStorage().objects().delete("v1/" + props.getProperty("swiftAccount") + "/" + container, objectName));
+		System.out.println(os.objectStorage().objects().delete("v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/cnt5699", "ProvaPUT"));
+	}
+	
+	//CAPIRE MEGLIO COME FUNZIONA
+	//COPIA DELL'OGGETTO DA UN CONTAINER ALL'ALTRO
+	public void CopiaOggetto(OSClient os)
+	{
+		String etag = os.objectStorage().objects().copy(ObjectLocation.create("v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/cnt569", "ProvaPUT"), 
+														ObjectLocation.create("v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/ppppp", "SCRIPTCIRROS"));
+		System.out.println("\nOggetto Copiato\n "+etag);
 	}
 	
 	//DOWNLOAD DEL FILE DAL CONTAINER
-	public void downloadFile(OSClient os, String container, String objectName, String destPath) throws IOException
+	public void downloadFile(OSClient os) throws IOException
 	{
-		ConfigurationProperties props = new ConfigurationProperties();
-		props.getProperty("swiftAccount");
-		DLPayload scaricato = os.objectStorage().objects().download("v1/" + props.getProperty("swiftAccount") + "/" + container, objectName);
-		File file = new File(destPath);
-		scaricato.writeToFile(file);
+		DLPayload scaricato = os.objectStorage().objects().download("v1/AUTH_34cbe4ae849b4ed2b47c40d391d69039/ppppp", "SCRIPTCIRROS");
+		System.out.println("\nFile Scaricato: \n"+scaricato.toString());
+		File file = new File("prova.txt");
 		FileReader leggi =new FileReader(file);
 		BufferedReader b = new BufferedReader(leggi);
 		String s;
